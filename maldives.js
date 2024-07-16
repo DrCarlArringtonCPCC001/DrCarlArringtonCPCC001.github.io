882311954c2e86909194df2d4ba6f57f1eec68f8
DrCarlArringtonCPCC001 <carl.arrington@cpcc.edu>
2024-07-16 14:16:52 -0400

    Update maldives.js

--- maldives.js
+++ maldives.js
@@ -8,3 +8,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// rangali hotel: Coordinates: 3°36′59″N 72°42′59″E -> 3.3659, 72.4259
+L.circle([3.3659, 72.4259], {
+	fillColor: 'teal',
+	color: 'black',
+	radius: 1000
+}).addTo(map);
