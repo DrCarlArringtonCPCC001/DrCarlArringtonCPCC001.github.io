9da367a3a4ddae13b4c6b90d5c78a1957e3ea5e2
DrCarlArringtonCPCC001 <carl.arrington@cpcc.edu>
2024-07-16 13:59:10 -0400

    Update maldives.js

--- maldives.js
+++ maldives.js
@@ -1,1 +1,10 @@
+// 3.2028° N, 73.2207° E -> 3.2028, 73.2207
+let map = L.map("map", {
+  center: [3.2028, 73.2207],
+  zoom: 15
+});
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
